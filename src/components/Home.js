f0f815747a66c50d37553854627a43d8e313e141
Savan <githost.2007@gmail.com>
2024-08-16 18:14:46 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React from "react";
 import SlidingComponent2 from './SlidingComponent2';
 const images = [
-    { src: "assets/images/slider/slider-1.jpg", alt: 'Image 1' },
-    { src: "assets/images/slider/image_02.jpg", alt: 'Image 2' },
-        { src: "assets/images/slider/slider-3.jpg", alt: 'Image 3' }
+    { src: "assets/images/slider-1.jpg", alt: 'Image 1' },
+    { src: "assets/images/image_02.jpg", alt: 'Image 2' },
+        { src: "assets/images/slider-3.jpg", alt: 'Image 3' }
                 ];
 const Home =()=>{
   return(
