a961f994906ee2f7821c4d19828798045a7c1787
Savan <githost.2007@gmail.com>
2024-08-17 20:38:00 +0530

    Update Programs.js

--- src/components/Programs.js
+++ src/components/Programs.js
@@ -15,6 +15,33 @@ import showimg11 from './static/Images/IMG-20240812-WA0022.jpg';
 import showimg14 from './static/Images/IMG-20240812-WA0018.jpg';
 import showimg15 from './static/Images/IMG-20240812-WA0016.jpg';
 import showimg16 from './static/Images/IMG-20240812-WA0014.jpg';
+import showimg17 from './static/Images/IMG-20240815-WA0055.jpg';
+import showimg18 from './static/Images/IMG-20240815-WA0053.jpg';
+import showimg19 from './static/Images/IMG-20240815-WA0051.jpg';
+ import showimg20 from './static/Images/IMG-20240815-WA0049.jpg';
+ import showimg21 from './static/Images/IMG-20240815-WA0047.jpg';
+ import showimg22 from './static/Images/IMG-20240815-WA0045.jpg';
+ import showimg23 from './static/Images/IMG-20240815-WA0043.jpg';
+import showimg24 from './static/Images/IMG-20240815-WA0041.jpg';
+import showimg25 from './static/Images/IMG-20240815-WA0042.jpg';
+import showimg26 from './static/Images/IMG-20240815-WA0039.jpg';
+import showimg27 from './static/Images/IMG-20240815-WA0037.jpg';
+import showimg28 from './static/Images/IMG-20240815-WA0035.jpg';
+import showimg29 from './static/Images/IMG-20240815-WA0033.jpg';
+ import showimg30 from './static/Images/IMG-20240815-WA0031.jpg';
+ import showimg31 from './static/Images/IMG-20240815-WA0029.jpg';
+import showimg32 from './static/Images/IMG-20240815-WA0028.jpg';
+import showimg33 from './static/Images/IMG-20240815-WA0025.jpg';
+import showimg34 from './static/Images/IMG-20240815-WA0023.jpg';
+import showimg35 from './static/Images/IMG-20240815-WA0021.jpg';
+import showimg36 from './static/Images/IMG-20240815-WA0019.jpg';
+import showimg37 from './static/Images/IMG-20240815-WA0017.jpg';
+import showimg38 from './static/Images/IMG-20240815-WA0015.jpg';
+import showimg39 from './static/Images/IMG-20240815-WA0013.jpg';
+import showimg40 from './static/Images/IMG-20240815-WA0011.jpg';
+import showimg41 from './static/Images/IMG-20240815-WA0009.jpg';
+ import showimg42 from './static/Images/IMG-20240815-WA0007.jpg';
+ import showimg43 from './static/Images/IMG-20240815-WA0005.jpg';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 const Programs =()=>{
@@ -164,6 +191,249 @@ const Programs =()=>{
         <div className="card">
                   <div className="show-scale-img">
           <LazyLoadImage effect="blur" src={showimg16} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg17} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg18} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg19} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg20} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg21} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg22} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg23} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg24} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg25} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg26} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg27} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg28} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg29} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg30} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg31} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg32} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg33} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg34} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+        <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg35} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg36} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg37} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg38} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg39} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg40} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg41} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg42} className="card-img-show lazy-load-image full-width" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+            <div className="col-6">
+      <div className="d-flex justify-content-center align-items-center">
+        <div className="card">
+                  <div className="show-scale-img">
+          <LazyLoadImage effect="blur" src={showimg43} className="card-img-show lazy-load-image full-width" alt="..." />
           </div>
         </div>
       </div>
